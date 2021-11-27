273574b217ddf13b481ce8b3a64c0d0cf5356ab3
samuel <samuel.teklay2001@gmail.com>
2021-11-27 03:16:51 +0100

    filling App.js with the main folders

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
+import Auth from './Pages/Auth/Auth';
+import Nav from './components/Nav/Nav';
+import Home from './Pages/Home/Home';
+import Footer from './components/Footer/Footer';
+
 import './App.css';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Nav />
+      <Auth />
+      <Home />
+      <Footer />
     </div>
   );
-}
+};
 
 export default App;
